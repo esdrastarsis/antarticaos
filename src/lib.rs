d0110bf9b337ebd918254638bd735a88ac37c65d
Esdras Tarsis <esdrastarsis@gmail.com>
2017-08-26 15:41:33 -0300

    Bugs Corrigidos na Compilação

---
+++ src/lib.rs
@@ -0,0 +1,23 @@
+#![feature(lang_items)]
+#![feature(const_fn)]
+#![feature(unique)]
+#![no_std]
+
+extern crate rlibc;
+extern crate volatile;
+extern crate spin;
+
+#[macro_use]
+mod vga_buffer;
+
+#[no_mangle]
+pub extern fn rust_main() {
+
+    vga_buffer::clear_screen();
+    println!("Bem Vindo ao AntarticaOS{}", "!");
+
+    loop {}
+}
+
+#[lang = "eh_personality"] extern fn eh_personality() {}
+#[lang = "panic_fmt"] #[no_mangle] pub extern fn panic_fmt() -> ! {loop{}}
