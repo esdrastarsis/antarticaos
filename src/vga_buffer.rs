0c0a56e6e3d8aedf9aa55aef13a789d0d5ad6c7b
Esdras Tarsis <esdrastarsis@gmail.com>
2017-08-26 17:45:01 -0300

    Correções no vga_buffer e Leia-me Atualizado

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -6,29 +6,29 @@ use volatile::Volatile;
 pub static WRITER: Mutex<Writer> = Mutex::new(Writer {
     column_position: 0,
     color_code: ColorCode::new(Color::LightGreen, Color::Black),
-    buffer: unsafe { Unique::new_unchecked(0xb0000 as *mut _) },
+    buffer: unsafe { Unique::new_unchecked(0xb8000 as *mut _) },
 });
 
 #[allow(dead_code)]
 #[derive(Debug, Clone, Copy)]
 #[repr(u8)]
 pub enum Color {
-    Black       = 0,
-    Blue        = 1,
-    Green       = 2,
-    Cyan        = 3,
-    Red         = 4,
-    Magenta     = 5,
-    Brown       = 6,
-    LightGray   = 7,
-    DarkGray    = 8,
-    LightBlue   = 9,
-    LightGreen  = 10,
-    LightCyan   = 11,
-    LightRed    = 12,
-    Pink        = 13,
-    Yellow      = 14,
-    White       = 15,
+    Black      = 0,
+    Blue       = 1,
+    Green      = 2,
+    Cyan       = 3,
+    Red        = 4,
+    Magenta    = 5,
+    Brown      = 6,
+    LightGray  = 7,
+    DarkGray   = 8,
+    LightBlue  = 9,
+    LightGreen = 10,
+    LightCyan  = 11,
+    LightRed   = 12,
+    Pink       = 13,
+    Yellow     = 14,
+    White      = 15,
 }
 
 #[derive(Debug, Clone, Copy)]
@@ -61,7 +61,7 @@ pub struct Writer {
 }
 
 impl Writer {
-    pub fn write_byte(&mut self, byte: u8){
+    pub fn write_byte(&mut self, byte: u8) {
         match byte {
             b'\n' => self.new_line(),
             byte => {
@@ -74,8 +74,8 @@ impl Writer {
 
                 let color_code = self.color_code;
                 self.buffer().chars[row][col].write(ScreenChar {
-                        ascii_character: byte,
-                        color_code: color_code,
+                    ascii_character: byte,
+                    color_code: color_code,
                 });
                 self.column_position += 1;
             }
@@ -86,12 +86,12 @@ impl Writer {
         unsafe{ self.buffer.as_mut() }
     }
 
-    fn new_line(&mut self){
-        for row in 1..BUFFER_HEIGHT{
-            for col in 0..BUFFER_WIDTH{
+    fn new_line(&mut self) {
+        for row in 1..BUFFER_HEIGHT {
+            for col in 0..BUFFER_WIDTH {
                 let buffer = self.buffer();
                 let character = buffer.chars[row][col].read();
-                buffer.chars[row - 1][col].write(character);        
+                buffer.chars[row - 1][col].write(character);
             }
         }
         self.clear_row(BUFFER_HEIGHT-1);
@@ -111,8 +111,8 @@ impl Writer {
 
 impl fmt::Write for Writer {
     fn write_str(&mut self, s: &str) -> fmt::Result {
-        for byte in s.bytes(){
-            self.write_byte(byte)
+        for byte in s.bytes() {
+          self.write_byte(byte)
         }
         Ok(())
     }
@@ -126,7 +126,7 @@ macro_rules! print {
 
 macro_rules! println {
     ($fmt:expr) => (print!(concat!($fmt, "\n")));
-    ($fmt:expr, $($arg:tt)*) => (print!(concat!($fmt, "\n"), $($arg)*));   
+    ($fmt:expr, $($arg:tt)*) => (print!(concat!($fmt, "\n"), $($arg)*));
 }
 
 pub fn print(args: fmt::Arguments) {
@@ -134,7 +134,7 @@ pub fn print(args: fmt::Arguments) {
     WRITER.lock().write_fmt(args).unwrap();
 }
 
-pub fn clear_screen(){
+pub fn clear_screen() {
     for _ in 0..BUFFER_HEIGHT {
         println!("");
     }
